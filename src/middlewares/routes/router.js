310078722e00ae1e9b61b023e59d6d3640643daa
andrey Nemodruk <nemodruk.briz.ua>
2021-12-10 09:39:40 +0200

    add controller health

--- src/middlewares/routes/router.js
+++ src/middlewares/routes/router.js
@@ -1,14 +1,8 @@
 const Router = require('@koa/router');
 
 const router = new Router();
-const pJson = require('../../../package.json');
+const gatewayHealth = require('../../controllers/gatewayHealth');
 
-router.get('/gateway/health', async (ctx) => {
-  ctx.set({ 'Content-Type': 'application/json' });
-  ctx.status = 200;
-  ctx.body = JSON.stringify({
-    succes: `Name ${pJson.name}, version ${pJson.version}`
-  });
-});
+router.get('/gateway/health', gatewayHealth);
 
 module.exports = router;
